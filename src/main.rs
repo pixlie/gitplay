b1f65520af0123b16f125983c21aaae2696a4ab9
Sumit Datta <sumitdatta@gmail.com>
2021-07-14 16:42:29 +0530

    (task) Starting point to read the Git repository

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
+use std::env;
 #[macro_use]
 extern crate smart_default;
-
-use std::env;
-
 use iced::{
-    Column, Container, Element, HorizontalAlignment, Length, Sandbox, Settings, Text, text_input,
-    TextInput,
+    text_input, Application, Clipboard, Column, Command, Container, Element, HorizontalAlignment,
+    Length, Settings, Text, TextInput,
 };
-
 // use git2::{Repository};
 
 pub fn main() -> iced::Result {
@@ -35,7 +32,7 @@ struct RepositoryLoadingState {
     created_at: u32,
 }
 
-#[derive(Debug, Default)]
+#[derive(Clone, Debug, Default)]
 struct RepositoryReadyState {
     path_to_repository: String,
     current_commit_hash: String,
@@ -45,8 +42,7 @@ struct RepositoryReadyState {
     created_at: u32,
 }
 
-#[derive(Debug, Clone)]
-#[derive(SmartDefault)]
+#[derive(Clone, Debug, SmartDefault)]
 enum PlayState {
     #[default]
     Paused,
@@ -54,8 +50,7 @@ enum PlayState {
     ReachedEnd,
 }
 
-#[derive(Debug, Clone)]
-#[derive(SmartDefault)]
+#[derive(Debug, Clone, SmartDefault)]
 enum PlaybackSpeed {
     #[default]
     Normal,
@@ -71,16 +66,24 @@ enum PlaybackSpeed {
 enum Message {
     ChangeRepositoryPath(String),
     RepositoryPathSubmitted,
+    RepositoryLoaded(RepositoryReadyState),
+    RepositoryPlay,
+    RepositoryPause,
 }
 
-impl Sandbox for GitPlay {
+impl Application for GitPlay {
+    type Executor = iced::executor::Default;
     type Message = Message;
+    type Flags = ();
 
-    fn new() -> Self {
+    fn new(_flags: ()) -> (Self, Command<Message>) {
         // When this application loads, it loads with a default state
-        GitPlay::RepositoryNone(RepositoryNoneState {
-            ..RepositoryNoneState::default()
-        })
+        (
+            GitPlay::RepositoryNone(RepositoryNoneState {
+                ..RepositoryNoneState::default()
+            }),
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -98,31 +101,49 @@ impl Sandbox for GitPlay {
         String::from(format!("Showing git repository {}", selected_path))
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message, _clipboard: &mut Clipboard) -> Command<Message> {
         match self {
-            GitPlay::RepositoryNone(state) => {
-                match message {
-                    Message::ChangeRepositoryPath(path) => state.path_to_repository = path,
-                    Message::RepositoryPathSubmitted => {
-                        *self = GitPlay::RepositoryLoading(RepositoryLoadingState {
-                            path_to_repository: state.path_to_repository.clone(),
-                            ..RepositoryLoadingState::default()
-                        })
-                    }
+            GitPlay::RepositoryNone(state) => match message {
+                Message::ChangeRepositoryPath(path) => {
+                    state.path_to_repository = path;
+
+                    Command::none()
                 }
-            }
-            GitPlay::RepositoryLoading(_state) => (),
-            GitPlay::RepositoryReady(_state) => (),
-            GitPlay::RepositoryUnloading => ()
+                Message::RepositoryPathSubmitted => {
+                    *self = GitPlay::RepositoryLoading(RepositoryLoadingState {
+                        path_to_repository: state.path_to_repository.clone(),
+                        ..RepositoryLoadingState::default()
+                    });
+
+                    Command::none()
+                }
+                _ => {
+                    Command::none()
+                }
+            },
+
+            GitPlay::RepositoryLoading(_state) => match message {
+                Message::RepositoryLoaded(state) => {
+                    *self = GitPlay::RepositoryReady(state);
+
+                    Command::none()
+                },
+                _ => {
+                    Command::none()
+                }
+            },
+
+            // Default case handling, temporary code
+            _ => Command::none(),
         }
     }
 
     fn view(&mut self) -> Element<Message> {
         match self {
             GitPlay::RepositoryNone(RepositoryNoneState {
-                                        input_path_to_repository,
-                                        path_to_repository
-                                    }) => Container::new(
+                input_path_to_repository,
+                path_to_repository,
+            }) => Container::new(
                 Column::new()
                     .spacing(20)
                     .push(
@@ -137,47 +158,53 @@ impl Sandbox for GitPlay {
                             path_to_repository,
                             Message::ChangeRepositoryPath,
                         )
-                            .padding(15)
-                            .size(30)
-                            .on_submit(Message::RepositoryPathSubmitted)
+                        .padding(15)
+                        .size(30)
+                        .on_submit(Message::RepositoryPathSubmitted),
                     )
                     .push(
                         Text::new(path_to_repository.to_string())
                             .horizontal_alignment(HorizontalAlignment::Center)
                             .size(20),
-                    )
+                    ),
             )
-                .width(Length::Fill)
-                .height(Length::Fill)
-                .center_y()
-                .into(),
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_y()
+            .into(),
+
             GitPlay::RepositoryLoading(state) => Container::new(
-                Text::new(format!("Loading Git history of {}", state.path_to_repository))
-                    .horizontal_alignment(HorizontalAlignment::Center)
-                    .size(50),
+                Text::new(format!(
+                    "Loading Git history of {}",
+                    state.path_to_repository
+                ))
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .size(50),
             )
-                .width(Length::Fill)
-                .height(Length::Fill)
-                .center_y()
-                .into(),
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_y()
+            .into(),
+
             GitPlay::RepositoryReady(_state) => Container::new(
                 Text::new("Let's play!")
                     .horizontal_alignment(HorizontalAlignment::Center)
                     .size(50),
             )
-                .width(Length::Fill)
-                .height(Length::Fill)
-                .center_y()
-                .into(),
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_y()
+            .into(),
+
             GitPlay::RepositoryUnloading => Container::new(
                 Text::new("Bye bye")
                     .horizontal_alignment(HorizontalAlignment::Center)
                     .size(50),
             )
-                .width(Length::Fill)
-                .height(Length::Fill)
-                .center_y()
-                .into(),
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_y()
+            .into(),
         }
     }
 }
