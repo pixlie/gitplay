8175ed2b85846ec7a2ca53d4ab65a0f43d5776ed
Sumit Datta <sumitdatta@gmail.com>
2021-07-19 18:17:57 +0530

    (task) Trying out Git tree walk/iteration from a CLI software

---
+++ README.md
@@ -0,0 +1,12 @@
+# Welcome to gitplay!
+
+`gitplay` is a desktop app that you can point at a Git repository, and it will show you how the repository unfolds historically.
+I like to learn from existing projects, whatever the project or the language it is written in. But in many projects that are worth learning from, there is a long history.
+So I struggle to see how the project shaped up in time. What was the first commit of Redis like?
+Or at what point did Docker add `compose` - how does `compose` look like at the beginning?
+
+`gitplay` is a project under development, and a very slow pace of development.
+This is a learning project that I am using along with my friend to get easy with Rust, the programming language.
+I am still new to Rust so this will take some time.
+
+Thanks for checking, please feel free check back in some time. Either the project will grow, or I will give up on this learning effort. I hope it will grow. 
--- src/main.rs
+++ src/main.rs
@@ -1,251 +1,62 @@
-use std::env;
-#[macro_use]
-extern crate smart_default;
-use git2::Repository;
-use iced::{
-    text_input, Application, Clipboard, Column, Command, Container, Element, HorizontalAlignment,
-    Length, Settings, Text, TextInput,
-};
+use git2::{Commit, Repository, Tree, ObjectType};
 use std::default;
+use std::env;
 use std::fmt;
+use std::str;
 
-pub fn main() -> iced::Result {
-    GitPlay::run(Settings::default())
+fn main() {
+    let mut selected_path: String = "".to_string();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        selected_path.push_str(&args[1])
+    } else {
+        selected_path.push_str("/home/brainless/Projects/dwata");
+    }
+
+    println!("repository path {:?}", selected_path);
+    let repository: Repository = load_repository(&selected_path);
+    println!("repository opened");
+    show_first_commit(&repository)
 }
 
-enum GitPlayMachine {
-    RepositoryNone,
-    // RepositoryLoading,
-    RepositoryReady,
-    RepositoryUnloading,
+fn show_items_of_tree(tree: &Tree) {
+    for item in tree.iter() {
+        println!("{:?}", item.name().unwrap());
+    }
 }
 
-struct GitPlay {
-    current_state: GitPlayMachine,
-    input_path_to_repository: text_input::State,
-    path_to_repository: Option<String>,
-    repository: Option<Repository>,
-    current_commit_hash: Option<String>,
-    play_state: PlayState,
-    playback_speed: PlaybackSpeed,
-    total_commit_count: u32,
-    created_at: u32,
+fn get_tree_from_commit(commit: &Commit) -> &Tree {
+    &commit.tree().unwrap()
 }
 
-impl default::Default for GitPlay {
-    fn default() -> Self {
-        GitPlay {
-            current_state: GitPlayMachine::RepositoryNone,
-            input_path_to_repository: text_input::State::default(),
-            path_to_repository: None,
-            repository: None,
-            current_commit_hash: None,
-            play_state: PlayState::Paused,
-            playback_speed: PlaybackSpeed::Normal,
-            total_commit_count: 0,
-            created_at: 0,
+fn show_first_commit(repository: &Repository) {
+    let mut git_spec = "HEAD";
+
+    {
+        let tree_obj = repository.revparse_single(git_spec).unwrap();
+        let kind = tree_obj.kind().unwrap();
+
+        let oid = tree_obj.id();
+        println!("repository head Git ID {:?}", oid);
+        git_spec = oid.to_string().as_str();
+        println!("type is {:?}", kind);
+
+        match kind {
+            // ObjectType::Commit => show_items_of_tree(
+            //     get_tree_from_commit(
+            //         &tree_obj.as_commit().unwrap()
+            //     )
+            // ),
+            ObjectType::Tree => show_items_of_tree(tree_obj.as_tree().unwrap()),
+            _ => ()
         }
     }
 }
 
-#[derive(Clone, Debug, SmartDefault)]
-enum PlayState {
-    #[default]
-    Paused,
-    Playing,
-    ReachedEnd,
-}
-
-#[derive(Debug, Clone, SmartDefault)]
-enum PlaybackSpeed {
-    #[default]
-    Normal,
-    // One commit progress per 100 milliseconds
-    Half,
-    // Half the speed of Normal
-    Twice,
-    // Twice the speed of Normal
-    FourTimes,
-}
-
-#[derive(Clone)]
-enum Message {
-    ChangeRepositoryPath(String),
-    RepositoryPathSubmitted,
-    RepositoryLoaded,
-    RepositoryRewind,
-    RepositoryPlay,
-    RepositoryPause,
-}
-
-impl Application for GitPlay {
-    type Executor = iced::executor::Default;
-    type Message = Message;
-    type Flags = ();
-
-    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
-        // When this application loads, it loads with a default state
-        (
-            GitPlay {
-                ..GitPlay::default()
-            },
-            Command::none(),
-        )
-    }
-
-    fn title(&self) -> String {
-        let mut selected_path: String = "".to_string();
-        let args: Vec<String> = env::args().collect();
-
-        if args.len() == 1 {
-            selected_path.push_str(&args[0])
-        } else {
-            let cwd_result = env::current_dir().unwrap();
-            let cwd = cwd_result.to_str().unwrap();
-            selected_path.push_str(&cwd);
-        }
-
-        String::from(format!("Showing git repository {}", selected_path))
-    }
-
-    fn update(&mut self, message: Message, _clipboard: &mut Clipboard) -> Command<Message> {
-        match self.current_state {
-            GitPlayMachine::RepositoryNone => match message {
-                Message::ChangeRepositoryPath(path) => {
-                    self.path_to_repository = Option::Some(path);
-                }
-                Message::RepositoryPathSubmitted => {
-                    // Let us try to load the Git repository
-                    // self.current_state = GitPlayMachine::RepositoryLoading;
-
-                    match &self.path_to_repository {
-                        Some(path) => match load_repository(path.clone()) {
-                            Ok(repository) => {
-                                self.repository = Some(repository);
-                                self.current_state = GitPlayMachine::RepositoryReady;
-                            }
-                            Err(_) => {
-                                self.current_state = GitPlayMachine::RepositoryNone;
-                            }
-                        },
-                        None => (),
-                    }
-                }
-                _ => (),
-            },
-
-            // Default case handling, temporary code
-            _ => (),
-        }
-
-        Command::none()
-    }
-
-    fn view(&mut self) -> Element<Self::Message> {
-        match self.current_state {
-            GitPlayMachine::RepositoryNone => {
-                let path = match &self.path_to_repository {
-                    Some(path) => path.as_str(),
-                    None => "",
-                };
-
-                Container::new(
-                    Column::new()
-                        .spacing(20)
-                        .push(
-                            Text::new("Please select a repository")
-                                .horizontal_alignment(HorizontalAlignment::Center)
-                                .size(50),
-                        )
-                        .push(
-                            TextInput::new(
-                                &mut self.input_path_to_repository,
-                                "Path to a Git repository",
-                                path,
-                                Message::ChangeRepositoryPath,
-                            )
-                            .padding(15)
-                            .size(30)
-                            .on_submit(Message::RepositoryPathSubmitted),
-                        )
-                        .push(
-                            Text::new(path)
-                                .horizontal_alignment(HorizontalAlignment::Center)
-                                .size(20),
-                        ),
-                )
-                .width(Length::Fill)
-                .height(Length::Fill)
-                .center_y()
-                .into()
-            }
-
-            /*
-            GitPlayMachine::RepositoryLoading => Container::new(
-                Text::new(format!(
-                    "Loading Git history of {}",
-                    self.path_to_repository.unwrap()
-                ))
-                .horizontal_alignment(HorizontalAlignment::Center)
-                .size(50),
-            )
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center_y()
-            .into(),
-             */
-            GitPlayMachine::RepositoryReady => Container::new(
-                Text::new("Let's play!")
-                    .horizontal_alignment(HorizontalAlignment::Center)
-                    .size(50),
-            )
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center_y()
-            .into(),
-
-            GitPlayMachine::RepositoryUnloading => Container::new(
-                Text::new("Bye bye")
-                    .horizontal_alignment(HorizontalAlignment::Center)
-                    .size(50),
-            )
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center_y()
-            .into(),
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-enum RepositoryActionError {
-    LoadingError,
-}
-
-fn load_repository(path: String) -> Result<Repository, RepositoryActionError> {
+fn load_repository(path: &String) -> Repository {
     match Repository::open(path.clone()) {
-        Ok(repository) => Ok(repository),
-        Err(e) => {
-            println!("Got an error when trying to open repository {}", e);
-
-            Err(RepositoryActionError::LoadingError)
-        }
-    }
-}
-
-impl fmt::Debug for GitPlay {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let path = match &self.path_to_repository {
-            Some(path) => path.as_str(),
-            None => "",
-        };
-
-        write!(f, "{:?}", path)
-    }
-}
-
-impl fmt::Debug for Message {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Message")
+        Ok(repository) => repository,
+        Err(e) => panic!("Could not open repository"),
     }
 }
