025f91128667fd1d5b1eec61b3f658eb8fbb7656
Sumit Datta <sumitdatta@gmail.com>
2021-07-20 21:57:07 +0530

    (task) Start walking the Git revision graph

--- src/walker.rs
+++ src/walker.rs
@@ -1,50 +1,72 @@
 use git2::{Commit, ObjectType, Repository, Tree};
 
-struct WalkedTreeNode {
+#[derive(Debug)]
+struct GitNode {
     git_spec: String,
     kind: ObjectType,
     is_visited: bool,
-    parents: Vec<WalkedTreeNode>,
+    branches: Vec<GitNode>,
 }
 
-fn show_items_of_tree(tree: &Tree) {
+fn get_items_of_tree(tree: &Tree) -> Vec<GitNode> {
+    let mut nodes: Vec<GitNode> = Vec::new();
     for item in tree.iter() {
         println!("{:?}", item.name().unwrap());
+        nodes.push(GitNode {
+            git_spec: item.id().to_string(),
+            kind: item.kind().unwrap(),
+            is_visited: false,
+            branches: Vec::new(),
+        })
     }
+    nodes
 }
 
 pub fn walk_repository_from_head(repository: &Repository) {
-    let mut git_spec = "HEAD";
-
-    show_git_spec_details(repository, git_spec);
+    let node = GitNode {
+        git_spec: "HEAD".to_string(),
+        kind: ObjectType::Commit,
+        is_visited: false,
+        branches: Vec::new(),
+    };
+    let node = show_git_spec_details(repository, node);
+    println!("{:?}", node);
 }
 
-fn show_git_spec_details(repository: &Repository, git_spec: &str) {
-    {
-        println!("Git spec: {:?}", git_spec);
+fn show_git_spec_details(repository: &Repository, mut node: GitNode) -> GitNode {
+    let git_spec = node.git_spec.clone();
+    println!("Git spec: {:?}", git_spec);
 
-        match repository.revparse_single(git_spec) {
-            Ok(tree_obj) => {
-                let oid = tree_obj.id();
-                println!("ID id {:?} or kind {:?}", oid, tree_obj.kind());
+    match repository.revparse_single(git_spec.as_str()) {
+        Ok(tree_obj) => {
+            let oid = tree_obj.id();
+            println!("ID id {:?} or kind {:?}", oid, tree_obj.kind());
 
-                match tree_obj.kind() {
-                    Some(ObjectType::Tree) => show_items_of_tree(tree_obj.as_tree().unwrap()),
-                    Some(ObjectType::Commit) => match tree_obj.as_commit() {
+            match tree_obj.kind() {
+                Some(ObjectType::Tree) => {
+                    let mut branches = get_items_of_tree(tree_obj.as_tree().unwrap());
+                    node.branches.append(&mut branches);
+                }
+                Some(ObjectType::Commit) => {
+                    match tree_obj.as_commit() {
                         Some(commit) => {
                             match commit.tree() {
-                                Ok(t) => show_items_of_tree(&t),
-                                Err(_) => println!("Could not extract tree of commit")
+                                Ok(t) => {
+                                    let mut branches = get_items_of_tree(&t);
+                                    node.branches.append(&mut branches);
+                                }
+                                Err(_) => println!("Could not extract tree of commit"),
                             };
-                        },
-                        None => println!("Could not extract commit")
-                    },
-                    _ => println!("Could not get the kind for tree object"),
+                        }
+                        None => println!("Could not extract commit"),
+                    };
                 }
+                _ => println!("Could not get the kind for tree object"),
             }
-            Err(_) => println!("Could not revparse_single"),
-        };
-    }
+        }
+        Err(_) => println!("Could not parse the given revision specification"),
+    };
+    node
 }
 
 pub fn load_repository(path: &String) -> Repository {
