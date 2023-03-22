b04730c991cf012026023dd29c76b797a044488e
Sumit Datta <sumitdatta@gmail.com>
2023-03-22 10:26:49 +0530

    Removed Iced related code in Rust app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = createSignal("");
@@ -9,25 +7,12 @@ function App() {
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
+    setGreetMsg(await invoke("open_repository", { path: name() }));
   }
 
   return (
     <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-
+      <h1>Welcome to GitPlay!</h1>
       <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
 
       <div class="row">
